ed97a6a133d80cb23c6ab15d944558fe9f658173
PentiumIV <zhendosss777@gmail.com>
2024-08-17 00:48:24 +0300

    filament preheat profiles fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2269,11 +2269,11 @@
 #define PREHEAT_3_TEMP_CHAMBER 35
 #define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
 
-#define PREHEAT_3_LABEL       "TPU"
-#define PREHEAT_3_TEMP_HOTEND 230
-#define PREHEAT_3_TEMP_BED     50
-#define PREHEAT_3_TEMP_CHAMBER 35
-#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
+#define PREHEAT_4_LABEL       "TPU"
+#define PREHEAT_4_TEMP_HOTEND 220
+#define PREHEAT_4_TEMP_BED     70
+#define PREHEAT_4_TEMP_CHAMBER 35
+#define PREHEAT_4_FAN_SPEED   0 // Value from 0 to 255
 
 // @section motion
 
